9e88d74b108aa58e94de3528bfa7038f6abe9158
Ariel <ariel.joaquin11@gmail.com>
2023-06-14 20:32:46 -0300

    Fundations

---
+++ routes/fundationroutes.js
@@ -0,0 +1,30 @@
+import { Router, response } from "express";
+const fundationRoutes = Router();
+
+import connection from "../connectionToDB/index.js";
+
+fundationRoutes.get("/", (req,res) => {
+    const query = "SELECT id, name,CBU, collected FROM fundation";
+    connection.query(query, (err , response , fields) => {
+        if (err) throw err
+        res.send(response)
+    });
+});
+
+fundationRoutes.get("/:id", (req,res) => {
+    //me traigo lo que llega de un formulario del front
+    const id = req.body.id
+     //defino query
+    const query = `SELECT name , CBU , collected FROM fundation WHERE id=${id}`
+    //ejecuto query
+    connection.query(query, (err,response,fields)=>{
+        console.log("entro")
+        if(err){
+            throw err
+        }
+        //envio Rta al cliente
+        res.send(response)
+    })
+});
+
+export default fundationRoutes;
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,10 @@
 import {Router} from "express"
 const router = Router()
 import clientRoutes from "./clientroutes.js";
+import fundationRoutes from "./fundationroutes.js"
 
 router.use("/clients", clientRoutes);
+router.use("/fundations", fundationRoutes);
 
 export default router;
 
