ab2261144f6719cf8af47268ce8a33b0ab4e205b
Ragueb <matias_ketlun@yahoo.com.ar>
2023-06-19 15:26:19 -0300

    creacion de tabla donation

---
+++ routes/donation.js
@@ -0,0 +1,15 @@
+import { Router, response } from "express";
+const donationRoutes = Router();
+
+import connection from "../connectionToDB/index.js";
+
+donationRoutes.get("/", (req,res) => {
+    const query = "SELECT id, idPerson, idFundation, amount  FROM donation";
+    connection.query(query, (err , response , fields) => {
+        if (err) throw err
+        res.send(response)
+    });
+});
+
+
+export default donationRoutes;
--- routes/router.js
+++ routes/router.js
@@ -3,12 +3,12 @@ const router = Router()
 import personRoutes from "./personroutes.js";
 import fundationRoutes from "./fundationroutes.js"
 import rolroutes from "./rolroutes.js"
-
+import donationRoutes from "./donation.js";
 
 router.use("/persons", personRoutes);
 router.use("/fundations", fundationRoutes);
 router.use("/rols",rolroutes);
-
+router.use("/donation",donationRoutes);
 
 
 export default router;
