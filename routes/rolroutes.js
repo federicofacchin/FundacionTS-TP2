d26233cb017c15131d5d5e230da562a9f4dda872
Ragueb <matias_ketlun@yahoo.com.ar>
2023-06-19 12:51:50 -0300

    Creacion tabla roles

---
+++ routes/rolroutes.js
@@ -0,0 +1,43 @@
+import { Router, response } from "express";
+const rolRoutes = Router();
+
+import connection from "../connectionToDB/index.js";
+
+rolRoutes.get("/", (req,res) => {
+    const query = "SELECT id, name FROM rol";
+    connection.query(query, (err , response , fields) => {
+        if (err) throw err
+        res.send(response)
+    });
+});
+
+rolRoutes.get("/:id", (req,res) => {
+    //me traigo lo que llega de un formulario del front
+    const id = req.body.id
+     //defino query
+    const query = `SELECT id, name  FROM rol WHERE id=${id}`
+    //ejecuto query
+    connection.query(query, (err,response,fields)=>{
+        console.log("entro")
+        if(err){
+            console.log("error")
+            res.send("Usuario no encontrado")
+            throw err
+        }
+        //envio Rta al cliente
+        res.send(response)
+    })
+});
+
+rolRoutes.post("/", (req,res) => {
+    const {name} = req.body
+    const query = `INSERT INTO Rol(name) VALUES("${name}")`;
+    
+    connection.query(query, (err , response , fields) => {
+        if (err) throw err
+        res.send("rol creado exitosamente");
+    });
+});
+
+
+export default rolRoutes;
--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,14 @@ import {Router} from "express"
 const router = Router()
 import personRoutes from "./personroutes.js";
 import fundationRoutes from "./fundationroutes.js"
+import rolroutes from "./rolroutes.js"
+
 
 router.use("/persons", personRoutes);
 router.use("/fundations", fundationRoutes);
+router.use("/rols",rolroutes);
+
+
 
 export default router;
 
