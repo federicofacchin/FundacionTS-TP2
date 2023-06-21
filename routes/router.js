84f68dcdaab0c2e7049a7396a35ef883f60ea37c
Ragueb <matias_ketlun@yahoo.com.ar>
2023-06-21 19:40:09 -0300

    donationRoutes

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const router = Router()
 import personRoutes from "./personroutes.js";
 import fundationRoutes from "./fundationroutes.js"
 import rolroutes from "./rolroutes.js"
-import donationRoutes from "./donation.js";
+import donationRoutes from "./donationroutes.js";
 
 router.use("/persons", personRoutes);
 router.use("/fundations", fundationRoutes);
